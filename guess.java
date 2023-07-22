2dac490e1422aea06c77958a4ea91cb43c680eba
prajjwal2-3 <121849327+prajjwal2-3@users.noreply.github.com>
2023-07-22 23:37:49 +0530

    Update guess.java
    added comments

--- guess.java
+++ guess.java
@@ -3,11 +3,13 @@ import java.util.Scanner;
 
 public class guess {
     public static void main(String[] args) {
+         //size of array from which we'll choose number
         int[] arr= new int[10];
         for (int i = 0; i <arr.length; i++) {
        arr[i] = i;
         }
         Random gu = new Random();
+         //generating a random number
         int ran = gu.nextInt(arr[0],arr[9]);
         System.out.println("Guess the number");
         boolean but = true;
